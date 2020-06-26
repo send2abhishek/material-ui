274c8346535aeb641e55bcab8d536862c494fe7d
Kumar <Abhishek.Kumar3@tyson.com>
2020-06-26 17:35:39 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+// to overide the styles
+const useStyles = makeStyles({
+  helloThereStyle: {
+    fontSize: "90px",
+    color: "red",
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <Typography
+        variant="h6"
+        color="primary"
+        className={classes.helloThereStyle}
+      >
+        Abhishek Aryan
+      </Typography>
+      <Button variant="outlined" color="primary">
+        This is our first Button
+      </Button>
     </div>
   );
 }
