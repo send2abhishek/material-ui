4b795ded9f27be54954f4000ff172daae7d63ff3
Kumar <Abhishek.Kumar3@tyson.com>
2020-07-14 14:59:04 +0530

    dummy file

---
+++ src/components/dummy.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Dummy extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Dummy;
